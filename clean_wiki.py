51d0fcd662357d918fee9d175fa70b287d9006eb
Katherine Murphy <katwowmurph@gmail.com>
2021-11-10 18:03:13 -0500

    Data

--- clean_wiki.py
+++ clean_wiki.py
@@ -1,15 +1,23 @@
 
+ALL_TAGS = ['ar', 'ca', 'zh', 'da', 'nl', 'en', 'fr', 'de', 'el', 'it', 'ja', 'lt',
+                               'mk', 'pl', 'pt', 'ro', 'ru', 'es']
 
-def remove_lang_tag(tag, file_name):
+EN_RU_AR_ZH = ['en:', 'ru:', 'zh:', 'ar:']
+FILES = ['dev', 'test', 'train']
+
+def remove_lang_tag(tags, file_name):
 
     with open(file_name, 'r', encoding='UTF-8') as f:
         text = f.read()
-        text = text.replace(tag, "")
+        for tag in tags:
+            text = text.replace(tag+":", "")
         f.close()
         return text
 
 
 if __name__ == "__main__":
-    outtext = remove_lang_tag('en:', "./panx_dataset/select/en.tar.gz.out/dev")
-    with open("./panx_dataset/select/en.tar.gz.out/dev_notag", 'w', encoding='UTF-8') as w:
-        w.write(outtext)
+    for file in FILES:
+        for tag in ALL_TAGS:
+            outtext = remove_lang_tag(ALL_TAGS, "C:\Programming\Spacy\wikiann\langs\\"+tag+".tar.gz.out\\"+file)
+            with open("C:\Programming\Spacy\wikiann\langs\\"+tag+".tar.gz.out\\"+file+"_notag", 'w', encoding='UTF-8') as w:
+                w.write(outtext)
